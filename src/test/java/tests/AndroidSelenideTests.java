fcc0de35c4b9a3d9687f98fb0247ee653b1058d9
tmolonushenko <tmolonushenko@gmail.com>
2022-04-29 00:29:09 +0300

    added allure

--- src/test/java/tests/AndroidSelenideTests.java
+++ src/test/java/tests/AndroidSelenideTests.java
@@ -7,18 +7,22 @@ import tests.TestBase;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class AndroidSelenideTests extends TestBase {
 
     @Test
     void searchTest() {
 
-        $(MobileBy.AccessibilityId("Search Wikipedia")).click();
-        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
+        step("Type search", () -> {
+            $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
+        });
 
-        $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+        step("Verify content found", () ->
+        $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0)));
 
     }
 
 
-    }
+}
