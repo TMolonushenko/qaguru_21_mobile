e2ed4dbbb822304bb03d7ca4f276365291dc57f1
tmolonushenko <tmolonushenko@gmail.com>
2022-04-29 00:22:23 +0300

    added allure

---
+++ src/test/java/helpers/Browserstack.java
@@ -0,0 +1,22 @@
+package helpers;
+
+import io.restassured.response.Response;
+import org.codehaus.groovy.antlr.treewalker.SourcePrinter;
+
+import static io.restassured.RestAssured.given;
+
+public class Browserstack {
+
+    public static String videoUrl(String sessionId) {
+     return
+                given()
+                        .auth().basic("tpuretest_lSVWc2", "Ggd5nW19cyeGseGGn2m6")
+                        .when()
+                        .get("https://api-cloud.browserstack.com/app-automate/sessions/" + sessionId +".json")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .path("automation_session.video_url");
+
+    }
+}
