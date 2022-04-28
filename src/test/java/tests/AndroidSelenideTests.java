1ffb4414bdcaf74f3776128f825883d9a0b16833
tmolonushenko <tmolonushenko@gmail.com>
2022-04-29 00:36:56 +0300

    added allure listener

--- src/test/java/tests/AndroidSelenideTests.java
+++ src/test/java/tests/AndroidSelenideTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
 
@@ -9,6 +10,7 @@ import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
+@Tag("selenide")
 public class AndroidSelenideTests extends TestBase {
 
     @Test
